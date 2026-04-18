0df41547a5dc6b4128d2a5bb07b047a640c8b811
rudrapratap2104 <rudrpratap.study@gmail.com>
2026-04-18 10:20:33 +0000

    02_datatypes_done

--- 01_basics/02_datatypes.js
+++ 01_basics/02_datatypes.js
@@ -1,3 +1,11 @@
 "use strict"; // lets us use all new version of js code
 
-alert (hiii) // 
+// alert (hiii) // we are using node js not browser
+
+let name = "SM"
+let no = 101
+let youUp = true
+
+// symbol = unique
+// null = object (typeOf)  
+// undefined = undefined (typeOf)  
