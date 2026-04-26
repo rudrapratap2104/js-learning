9ad90acc5f683bcc12fb26f70fb0c75b1ddbb3fe
rudrapratap2104 <rudrpratap.study@gmail.com>
2026-04-26 07:37:34 +0000

    vid_15 done

---
+++ 02_basics/02_arrays.js
@@ -0,0 +1,31 @@
+const marvelHeroes = ["Iron Man" , "Spider-Man" , "Thor"]
+const dcHeroes = ["Batman" , "Superman" , "Aquaman"]
+
+// marvelHeroes.push(dcHeroes)
+
+// console.log(marvelHeroes);
+// // console.log(marvelHeroes[3][0]);
+
+// const allHeroes = marvelHeroes.concat(dcHeroes) // does not modify the original array
+
+// console.log(allHeroes);
+
+// const multiverse = [...marvelHeroes, ...dcHeroes]
+// console.log(multiverse);
+
+// const arr = [1, 2, 3, [1,2,3], 4, 5, [9.10, 11.12, ["kya", "yaar"]], 23, 13, ['hihi', 'hello']];
+// console.log(arr);
+
+// const usableArr = arr.flat(Infinity); // flattens the array to any depth
+// console.log(usableArr);
+
+
+
+console.log(Array.from("Rudra"));
+
+let str = "Rudra";
+let no = 02;
+let score = 99;
+
+console.log(Array.of(str, no, score));
+
